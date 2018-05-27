5299f3fe6f468f118d257e3c94016b4a7892a676
DingJunyao <dingjunyao0703@163.com>
2018-05-27 15:54:16 +0800

    login in index

--- taoshu/public/js/register.js
+++ taoshu/public/js/register.js
@@ -1,11 +1,3 @@
-function setCookie(c_name,value,expiredays)
-{
-    var exdate=new Date()
-    exdate.setDate(exdate.getDate()+expiredays)
-    document.cookie=c_name+ "=" +escape(value)+
-        ((expiredays==null) ? "" : ";expires="+exdate.toGMTString())
-}
-
 document.querySelector('form').addEventListener('submit',function(e){
     e.preventDefault();
 },false);
--- taoshu/routes/api.js
+++ taoshu/routes/api.js
@@ -17,13 +17,17 @@ router.post('/register', function(req, res, next) {
 });
 
 router.post('/login', function (req, res, next) {
-  var postInfo = {fldName: req.body.phone, fldPassword: req.body.password}
+  var postInfo = [req.body.username, req.body.password]
   pool.getConnection(function(err, connection){
-    connection.query('SELECT * FROM tbluser WHERE ?', postInfo, function(err, rows) {
+    connection.query('SELECT * FROM tbluser WHERE fldName = ? AND fldPassword = ?', postInfo, function(err, rows) {
       if(err){
          res.end("1");
       } else {
-         res.end("0");
+        if(rows[0] !== undefined) {
+          res.end("0");
+        } else {
+          res.end("-1");
+        }
       }
       connection.release();
     });
