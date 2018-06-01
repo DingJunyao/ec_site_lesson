a3c5ef16848cd40cf15a5605873b6bdc492e3717
DingJunyao <dingjunyao0703@163.com>
2018-06-01 09:52:13 +0800

    list finish

--- taoshu/routes/list.js
+++ taoshu/routes/list.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('list', {search: '', books: ''});
+  res.render('list', {search: '', rows: ''});
 });
 
 router.get('/type/:type', function(req, res, next) {
@@ -22,7 +22,40 @@ router.get('/type/:type', function(req, res, next) {
       connection.release();
     });
   });
+});
 
+router.get('/author/:author', function(req, res, next) {
+  pool.getConnection(function(err, connection){
+    connection.query('SELECT * FROM tblbook WHERE fldAuthor = ?', req.params.author, function(err, rows) {
+      if(err){
+         res.end("1");
+      } else {
+        if(rows[0] !== undefined) {
+          res.render('list', {search: '', rows: rows});
+        } else {
+          res.render('list', {search: '', rows: ''});
+        }
+      }
+      connection.release();
+    });
+  });
+});
+
+router.get('/press/:press', function(req, res, next) {
+  pool.getConnection(function(err, connection){
+    connection.query('SELECT * FROM tblbook WHERE fldPress = ?', req.params.press, function(err, rows) {
+      if(err){
+         res.end("1");
+      } else {
+        if(rows[0] !== undefined) {
+          res.render('list', {search: '', rows: rows});
+        } else {
+          res.render('list', {search: '', rows: ''});
+        }
+      }
+      connection.release();
+    });
+  });
 });
 
 module.exports = router;
